1259626adcf87954e7d53f3164157c9c68e61d84
Calum <calum.borders@gmail.com>
2024-06-18 13:54:29 +0100

    Got key data fetched from databse and rendered on the site. WIP - finish seeding database with data and style pages further

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -4,29 +4,68 @@ import { faArrowAltCircleLeft } from "@fortawesome/free-solid-svg-icons";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import Link from "next/link";
 import Image from "next/image";
-import testImage from "../../../public/TestImage.jpg";
 import { useEffect, useState } from "react";
-import { db } from "@/app/firebaseConfig";
+import { db, storage } from "@/app/firebaseConfig";
 import { get, ref } from "firebase/database";
 import { usePathname } from "next/navigation";
+import { getDownloadURL, ref as storageRef } from "firebase/storage";
+
+interface Project {
+  name: string;
+  date: string;
+  description: string;
+  info?: string;
+  comments?: string;
+  techUsed: string;
+  links?: { [key: string]: string };
+}
 
 export default function individualPage() {
-  const [pageProjet, setPageProject] = useState({})
+  const [pageProjet, setPageProject] = useState<Project>({} as Project);
+  const [imageURL, setImageURL] = useState("");
   const route = usePathname();
 
-  useEffect(() => {
+  const fetchIndividualProject = async (route: string) => {
+    const projectRef = ref(db, `${route}`);
+    const snapshot = await get(projectRef);
 
-    const projectRef = ref(db, `${route}`)
-    get(projectRef).then((snapshot) => {
-      if (snapshot.exists()){
-        const object = snapshot.val()
-        setPageProject(object)
+    try {
+      if (snapshot.exists()) {
+        const object = snapshot.val();
+        return object;
       }
-    })
+    } catch (error) {
+      console.error("Error fetching data", error);
+    }
+  };
 
-  }, [db])
+  const fetchImageUrl = async (imageName: string) => {
+    const imageRef = storageRef(storage, `project-images/${imageName}.png`);
 
- 
+    try {
+      const url = await getDownloadURL(imageRef);
+      return url;
+    } catch (error) {
+      console.error("Error fetching image", error);
+      return null;
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchIndividualProject(route);
+      setPageProject(data);
+
+      const url = await fetchImageUrl(data.name);
+      if (url) {
+        setImageURL(url);
+      } else {
+        console.error("Cannot find imageURL");
+      }
+    };
+
+    fetchData();
+  }, [db]);
 
   return (
     <>
@@ -39,48 +78,35 @@ export default function individualPage() {
       <main className="h-screen px-6 mx-auto space-y-8 max-w-7xl">
         <div className="max-w-full">
           <h2 className="text-5xl text-center font-bold my-8">
-            {"INSERT PROJECT NAME"}
+            {pageProjet.name}
           </h2>
         </div>
         <div className="w-full h-px bg-black dark:bg-white"></div>
 
         <div className="h-1/2 max-w-full relative">
-          <Image src={testImage} fill={true} alt="Image of Project" />
+          {imageURL ? (
+            <Image src={imageURL} fill={true} alt="Image of Project" />
+          ) : (
+            <p className="text-center">No image found </p>
+          )}
         </div>
         <div className="w-full h-px bg-black dark:bg-white"></div>
 
         <div>
           <p className="text-lg font-medium tracking-wide text-justify">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean id
-            risus id mi consequat viverra. Donec aliquet malesuada nisl sed
-            fermentum. Cras fringilla vestibulum diam at porta. Phasellus nibh
-            lorem, viverra ac leo et, eleifend aliquet massa. Donec lobortis
-            dapibus tellus, ac tincidunt nibh accumsan in. Sed fringilla
-            ultricies hendrerit. Praesent ac laoreet tortor. Ut volutpat ex
-            facilisis massa euismod, eu porttitor dui tincidunt. Phasellus ac
-            fermentum neque, sed suscipit nisi. Sed vel urna sit amet sem
-            efficitur vehicula ut id magna. Fusce sed neque purus. Maecenas
-            ultrices iaculis mattis. Curabitur pellentesque magna vitae luctus
-            porttitor. Morbi consectetur, enim vel sodales rutrum, ipsum felis
-            auctor diam, vitae aliquet libero ante at ipsum. Nam non turpis
-            vitae purus auctor sodales a ac mi. Nam dignissim tristique dolor
-            non rutrum. Duis sodales, nibh ut mollis aliquam, sem nunc lacinia
-            nulla, nec mattis dolor quam quis lorem. Etiam sed aliquam leo.
-            Donec consequat nunc at convallis tempor. Phasellus placerat rutrum
-            porta. Nam molestie maximus euismod. Curabitur eget viverra mi. Ut
-            quis urna eget est tincidunt posuere id vel nulla. Sed condimentum
-            felis a dapibus venenatis.
+            {pageProjet.description}
           </p>
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           <div className="grid-cols-1">
-            <h3 className="text-2xl font-bold text-center my-2">Tech Used:</h3>
+            <h3 className="text-2xl font-bold  my-2">Tech Used:</h3>
             <div className="w-full h-px bg-black dark:bg-white"></div>
 
-            <ul className="list-disc list-inside flex justify-evenly items-center h-full">
-              <li>JS</li>
-              <li>TS</li>
+            <ul className="list-disc list-inside items-center  p-2 h-full">
+              {pageProjet.techUsed?.split(" ").map((tech) => {
+                return <li>{tech}</li>;
+              })}
             </ul>
           </div>
 
@@ -90,10 +116,7 @@ export default function individualPage() {
             </h3>
             <div className="w-full h-px bg-black dark:bg-white"></div>
             <p className="text-md tracking-wide text-justify my-2 mx-2">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean id
-              risus id mi consequat viverra. Donec aliquet malesuada nisl sed
-              fermentum. Cras fringilla vestibulum diam at porta. Phasellus nibh
-              lorem, viverra ac leo et, eleifend aliquet massa.
+              {pageProjet.comments}
             </p>
           </div>
         </div>
@@ -102,8 +125,20 @@ export default function individualPage() {
         <div>
           <h3 className="text-center">Project Links</h3>
           <div className="flex justify-evenly">
-            <a>Github</a>
-            <a>Hosting</a>
+            {pageProjet.links ? (
+              Object.keys(pageProjet.links).map((key) => (
+                <a
+                  key={key}
+                  href={pageProjet.links?.[key]}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {key}
+                </a>
+              ))
+            ) : (
+              <p>No Links Avaliable</p>
+            )}
           </div>
         </div>
       </main>
